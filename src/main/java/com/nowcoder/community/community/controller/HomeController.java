36e2d821ae33ea2cd623605d407d93ecafdf1174
hanyiyu <46866888+hanyiyu@users.noreply.github.com>
2022-12-28 21:37:11 -0800

    update Dec 27

--- src/main/java/com/nowcoder/community/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/community/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.community.controller;
 
 
 import com.nowcoder.community.community.entity.DiscussPost;
+import com.nowcoder.community.community.entity.Page;
 import com.nowcoder.community.community.entity.User;
 
 import com.nowcoder.community.community.service.DiscussPostService;
@@ -26,9 +27,12 @@ public class HomeController {
     private UserService userService;
 
     @RequestMapping(path = "/index",method = RequestMethod.GET)
-    public String getIndexPage(Model model){
+    public String getIndexPage(Model model, Page page){
 
-        List<DiscussPost> list = discussPostService.findDiscussPosts(0, 0, 10);
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String, Object>> discussPosts = new ArrayList<>();
         if (list != null) {
             for (DiscussPost post : list) {
