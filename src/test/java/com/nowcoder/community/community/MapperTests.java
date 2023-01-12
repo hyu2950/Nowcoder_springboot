fe5c37d7b8aeb169ca76c3314dfc0e790f866286
hanyiyu <hanyiyu@usc.edu>
2023-01-11 20:25:50 -0800

    update login jan 11

--- src/test/java/com/nowcoder/community/community/MapperTests.java
+++ src/test/java/com/nowcoder/community/community/MapperTests.java
@@ -2,8 +2,10 @@ package com.nowcoder.community.community;
 
 
 import com.nowcoder.community.community.dao.DiscussPostMapper;
+import com.nowcoder.community.community.dao.LoginTicketMapper;
 import com.nowcoder.community.community.dao.UserMapper;
 import com.nowcoder.community.community.entity.DiscussPost;
+import com.nowcoder.community.community.entity.LoginTicket;
 import com.nowcoder.community.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -22,6 +25,8 @@ public class MapperTests {
     private DiscussPostMapper discussPostMapper;
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     @Test
     public void testSelectUser() {
@@ -44,5 +49,26 @@ public class MapperTests {
             System.out.println(post);
         }
 
+    }
+
+    @Test
+    public void testInsertLoginTicket(){
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket() {
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc", 1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
     }
 }
