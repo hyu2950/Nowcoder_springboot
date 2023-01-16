97874dfcb0cd18dd8de758e008072737f4cc4cdb
hanyiyu <hanyiyu@usc.edu>
2023-01-15 16:56:07 -0800

    update setting

---
+++ src/main/java/com/nowcoder/community/community/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.nowcoder.community.community.controller;
+
+import com.nowcoder.community.community.service.UserService;
+import com.nowcoder.community.community.util.HostHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+
+    @RequestMapping(path = "/setting", method = RequestMethod.GET)
+    public String getSettingPage() {
+        return "/site/setting";
+    }
+
+    @RequestMapping(path = "/upload",method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model){
+        if (headerImage == null){
+            model.addAttribute("error","您还没有选择图片！");
+            return "/site/setting";
+        }
+        String fileName = headerImage.getOriginalFilename();
+        fileName.substring(fileName.lastIndexOf("."));
+
+    }
+}
